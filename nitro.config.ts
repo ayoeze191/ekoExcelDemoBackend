abce2855037c00ea8f0d0a4751650828ad20fb5b
ayoeze191 <ayoeze191@gmail.com>
2025-01-27 00:13:08 -0500

    scam

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,6 +2,7 @@
 export default defineNitroConfig({
   plugins: ["plugins/mongoose.ts", "middleware/cors.ts"],
   srcDir: "server",
+  preset: "netlify",
   compatibilityDate: "2025-01-21",
   experimental: {
     openAPI: true,
